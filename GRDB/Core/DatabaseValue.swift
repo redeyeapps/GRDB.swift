9e88c4afce339c94f85051f5731c70c43946d5d1
Arash Daneshvar <arash01@gmail.com>
2019-03-15 11:13:26 +1000

    fix(Convert): Checks if expected string value contains only digits

--- GRDB/Core/DatabaseValue.swift
+++ GRDB/Core/DatabaseValue.swift
@@ -275,6 +275,9 @@ extension DatabaseValue {
         if let value = T.fromDatabaseValue(self) {
             return value
         }
+        if T.self is String, let value = Int64.fromDatabaseValue(self) {
+            return String(value) as! T
+        }
         if isNull {
             // Failed conversion from null: ok
             return nil
